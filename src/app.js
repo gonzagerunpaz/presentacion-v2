a23380ae30a03189d3fb206d0f75edaca15eb006
Gonzalez <a446857@lbr000258.rio.ar.bsch>
2023-03-13 20:37:30 -0300

    forEach, filter, map y reducer

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log('Hola Mundo!!!');
+const vec = [2, 9, 3, 34, 14]
+
+// Los voy a iterar y mostrar en pantalla
+vec.forEach( num => console.log(num));
+
+console.log('')
+
+// Como voy a Filtar los numeros pares
+const vecFiltrado = vec.filter( num => num % 2 == 0 )
+console.log(vecFiltrado)
+
+console.log('')
+
+// Para transformar
+const vecTransformado = vec.map ( num => num * 2 )
+console.log(vecTransformado)
+
+console.log('')
+
+// Utilizar un reductor
+console.log( vec.reduce( (a,b)  => a + b, 10))
+
